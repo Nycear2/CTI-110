d8859ce17c322d67eb00fc1fbd458514b83112ca
Nycear2 <150168985+Nycear2@users.noreply.github.com>
2023-11-07 11:21:51 -0500

    First Upload

---
+++ P1LAB1_NycearCarroll.py
@@ -0,0 +1,9 @@
+#FirstName LastName
+#11/7/2023
+#Basic Imput/Output
+
+#Get Input from user
+name = input("Please enter your name: ")
+
+#Display output to the user
+print("Hello", name, "Welome to CTI-110!")
